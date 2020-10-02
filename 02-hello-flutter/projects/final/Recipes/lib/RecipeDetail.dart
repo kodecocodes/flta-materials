39f4762d519c225f3a4e7480f74fe7d3cc4d94d1
Michael Katz <mikekatz@users.noreply.github.com>
2020-10-02 15:53:28 -0400

    finalize

--- 02-hello-flutter/projects/final/Recipes/lib/RecipeDetail.dart
+++ 02-hello-flutter/projects/final/Recipes/lib/RecipeDetail.dart
@@ -1,112 +1,98 @@
-  import 'package:flutter/material.dart';
-  import 'recipe.dart';
+/*
+* Copyright (c) 2020 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+* distribute, sublicense, create a derivative work, and/or sell copies of the
+* Software in any work that is designed, intended, or marketed for pedagogical or
+* instructional purposes related to programming, coding, application development,
+* or information technology.  Permission for such use, copying, modification,
+* merger, publication, distribution, sublicensing, creation of derivative works,
+* or sale is expressly withheld.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
-  class RecipeDetail extends StatefulWidget {
-    final Recipe recipe;
+import 'package:flutter/material.dart';
+import 'recipe.dart';
 
-    RecipeDetail(this.recipe);
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
 
-    @override
-    _RecipeDetailState createState() {
-      return _RecipeDetailState(recipe);
-    }
+  RecipeDetail(this.recipe);
+
+  @override
+  _RecipeDetailState createState() {
+    return _RecipeDetailState(recipe);
   }
+}
 
-  class _RecipeDetailState extends State {
-    final Recipe recipe;
-    int _sliderVal = 1;
+class _RecipeDetailState extends State {
+  final Recipe recipe;
+  int _sliderVal = 1;
 
-    _RecipeDetailState(this.recipe);
+  _RecipeDetailState(this.recipe);
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text(recipe.label),
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Image(image: AssetImage(recipe.imageUrl)),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(recipe.label),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image(image: AssetImage(recipe.imageUrl)),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              recipe.label,
+              style: TextStyle(fontSize: 18),
+            ),
+           Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(7.0),
+                  itemCount: recipe.ingredients.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final ingredient = recipe.ingredients[index];
+                    return Text("${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}");
+                  }
               ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                recipe.label,
-                style: TextStyle(fontSize: 18),
-              ),
-             Expanded(
-                child: ListView.builder(
-                    padding: const EdgeInsets.all(7.0),
-                    itemCount: recipe.ingredients.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final ingredient = recipe.ingredients[index];
-                      return Text("${ingredient.quantity} ${ingredient.measure} ${ingredient.name}");
-                    }
-                ),
-              ),
-            ],
-          ));
-    }
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 10,
+              label: "${_sliderVal*recipe.servings} servings",
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            )
+          ],
+        ));
   }
-
-  //class _RecipeDetailState2 extends State {
-  //  final Recipe recipe;
-  //  int _sliderVal = 1;
-  //
-  //  _RecipeDetailState(this.recipe);
-  //
-  //  @override
-  //  Widget build(BuildContext context) {
-  //    return Scaffold(
-  //        appBar: AppBar(
-  //          title: Text(recipe.label),
-  //        ),
-  //        body: Column(
-  //          mainAxisSize: MainAxisSize.max,
-  //          children: <Widget>[
-  //            Container(
-  //              height: 300,
-  //              width: double.infinity,
-  //              child: Image(image: AssetImage(recipe.imageUrl)),
-  //            ),
-  //            SizedBox(
-  //              height: 4,
-  //            ),
-  //            Text(
-  //              recipe.label,
-  //              style: TextStyle(fontSize: 18),
-  //            ),
-  //            new Expanded(
-  //              child:
-  //              ListView.builder(
-  //                  padding: const EdgeInsets.all(7.0),
-  //                  itemCount: recipe.ingredients.length,
-  //                  itemBuilder: (BuildContext context, int index) {
-  //                    final ingredient = recipe.ingredients[index];
-  //                    return Text("${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}");
-  //                  }
-  //              ),
-  //            ),
-  //            Slider(
-  //              min: 1,
-  //              max: 10,
-  //              divisions: 10,
-  //              label: "${_sliderVal*recipe.servings} servings",
-  //              value: _sliderVal.toDouble(),
-  //              onChanged: (newValue) {
-  //                setState(() {
-  //                  _sliderVal = newValue.round();
-  //                });
-  //              },
-  //              semanticFormatterCallback: (double newValue) {
-  //                return '${newValue.round()} dollars';
-  //              },
-  //            ),
-  //          ],
-  //        ));
-  //  }
-  //}
+}
--- 02-hello-flutter/projects/final/Recipes/lib/main.dart
+++ 02-hello-flutter/projects/final/Recipes/lib/main.dart
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import 'package:flutter/material.dart';
 import 'recipe.dart';
 import 'RecipeDetail.dart';
@@ -27,15 +57,6 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
