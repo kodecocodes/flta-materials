6f23012f699a59ba8383cdcd05b03a08a8c83c2c
Stef-GMS <GeekMeSpeak@gmail.com>
2020-10-09 19:01:40 -0700

    TE updates to match chapter edits

--- 02-hello-flutter/projects/final/Recipes/lib/main.dart
+++ 02-hello-flutter/projects/final/Recipes/lib/main.dart
@@ -29,15 +29,17 @@
  */
 
 import 'package:flutter/material.dart';
+
 import 'recipe.dart';
-import 'RecipeDetail.dart';
+import 'recipe_detail.dart';
 
 void main() {
-  runApp(RecipeApp());
+  runApp(MyApp());
 }
 
-class RecipeApp extends StatelessWidget {
-  // 1
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+// 1
   @override
   Widget build(BuildContext context) {
     // 2
@@ -45,10 +47,9 @@ class RecipeApp extends StatelessWidget {
       // 3
       title: 'Recipe Calculator',
       theme: ThemeData(
-        // 4
+          // 4
           primaryColor: Colors.white,
-          accentColor: Colors.black
-      ),
+          accentColor: Colors.black),
       // 5
       home: MyHomePage(title: 'Recipe Calculator'),
     );
@@ -67,62 +68,73 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // 1
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      return RecipeDetail(Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
         ),
-        body: ListView.builder(
-            itemCount: Recipe.samples.length,
-            itemBuilder: (BuildContext context, int index) {
-              // 1
-              return GestureDetector(
-                // 2
-                onTap: () {
-                  // 3
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (context) {
-                        // 4
-                        return RecipeDetail(Recipe.samples[index]);
-                      }
-                  ));
-                },
-                // 5
-                child: buildRecipeCard(Recipe.samples[index]),
-              );
-            }
-        )
+      ),
     );
   }
-}
 
-Widget buildRecipeCard(Recipe recipe) {
-  return Card(
-    // 1
-    elevation: 2.0,
-    // 2
-    shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0)
-    ),
-    // 3
-    child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      // 4
-      child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          // 5
-          SizedBox(
-            height: 14.0,
-          ),
-          Text(recipe.label,
-              // 6
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            // 5
+            SizedBox(
+              height: 14.0,
+            ),
+            // 6
+            Text(
+              recipe.label,
               style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Palatino"
-              ))
-        ],
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: "Palatino",
+              ),
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
--- 02-hello-flutter/projects/final/Recipes/lib/recipe.dart
+++ 02-hello-flutter/projects/final/Recipes/lib/recipe.dart
@@ -31,35 +31,70 @@
 class Recipe {
   String label;
   String imageUrl;
-
   int servings;
   List<Ingredient> ingredients;
 
   Recipe(this.label, this.imageUrl, this.servings, this.ingredients) {}
 
   static List<Recipe> samples = [
-    Recipe("Spaghetti and Meatballs", "assets/2126711929_ef763de2b3_w.jpg", 4,
-        [Ingredient(1, "box", "Spaghetti"),
-          Ingredient(4, "", "Frozen Meatballs"),
-          Ingredient(0.5, "jar", "sauce")]),
-    Recipe("Tomato Soup", "assets/27729023535_a57606c1be.jpg", 2,
-        [Ingredient(1, "can", "Tomato Soup")]),
-    Recipe("Grilled Cheese", "assets/3187380632_5056654a19_b.jpg", 1,
-        [Ingredient(2, "slices", "Cheese"),
-          Ingredient(2, "slices", "Bread")]),
-    Recipe("Chocolate Chip Cookies", "assets/15992102771_b92f4cc00a_b.jpg", 24,
-        [Ingredient(4, "cups", "flour"),
-          Ingredient(2, "cups", "sugar"),
-          Ingredient(0.5, "cups", "chocolate chips")]),
-    Recipe("Taco Salad", "assets/8533381643_a31a99e8a6_c.jpg", 1,
-        [Ingredient(4, "oz", "nachos"),
-          Ingredient(3, "oz", "taco meat"),
-          Ingredient(0.5, "cup", "cheese"),
-          Ingredient(0.25, "cup", "chopped tomatoes")]),
-    Recipe("Hawaiian Pizza", "assets/15452035777_294cefced5_c.jpg", 4,
-        [Ingredient(1, "item", "pizza"),
-          Ingredient(1, "cup", "pineapple"),
-          Ingredient(8, "oz", "ham")]),
+    Recipe(
+      "Spaghetti and Meatballs",
+      "assets/2126711929_ef763de2b3_w.jpg",
+      4,
+      [
+        Ingredient(1, "box", "Spaghetti"),
+        Ingredient(4, "", "Frozen Meatballs"),
+        Ingredient(0.5, "jar", "sauce"),
+      ],
+    ),
+    Recipe(
+      "Tomato Soup",
+      "assets/27729023535_a57606c1be.jpg",
+      2,
+      [
+        Ingredient(1, "can", "Tomato Soup"),
+      ],
+    ),
+    Recipe(
+      "Grilled Cheese",
+      "assets/3187380632_5056654a19_b.jpg",
+      1,
+      [
+        Ingredient(2, "slices", "Cheese"),
+        Ingredient(2, "slices", "Bread"),
+      ],
+    ),
+    Recipe(
+      "Chocolate Chip Cookies",
+      "assets/15992102771_b92f4cc00a_b.jpg",
+      24,
+      [
+        Ingredient(4, "cups", "flour"),
+        Ingredient(2, "cups", "sugar"),
+        Ingredient(0.5, "cups", "chocolate chips"),
+      ],
+    ),
+    Recipe(
+      "Taco Salad",
+      "assets/8533381643_a31a99e8a6_c.jpg",
+      1,
+      [
+        Ingredient(4, "oz", "nachos"),
+        Ingredient(3, "oz", "taco meat"),
+        Ingredient(0.5, "cup", "cheese"),
+        Ingredient(0.25, "cup", "chopped tomatoes"),
+      ],
+    ),
+    Recipe(
+      "Hawaiian Pizza",
+      "assets/15452035777_294cefced5_c.jpg",
+      4,
+      [
+        Ingredient(1, "item", "pizza"),
+        Ingredient(1, "cup", "pineapple"),
+        Ingredient(8, "oz", "ham"),
+      ],
+    ),
   ];
 }
 
