c504dd1a1148c776d2fa94d1b8b5a91620dc6be3
Vincent Ngo <vincentvyngo@yahoo.com>
2020-10-23 13:26:16 +0700

    add license headers, and comments

--- 05-scrollable-widgets/projects/final/lib/api/mock_fooderlich_service.dart
+++ 05-scrollable-widgets/projects/final/lib/api/mock_fooderlich_service.dart
@@ -1,8 +1,41 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:fooderlich/models/models.dart';
 
+// Mock recipe service that grabs sample json data to mock recipe request/response
 class MockFooderlichService {
+
+  // Get sample explore recipes json to display in ui
   Future<List<ExploreRecipe>> getTodayRecipes() async {
     String dataString = await _loadAsset('assets/sample_data/sample_explore_recipes.json');
     Map<String, dynamic> json = jsonDecode(dataString);
@@ -18,6 +51,7 @@ class MockFooderlichService {
     }
   }
 
+  // Get the sample friend json posts to display in ui
   Future<List<Post>> getFriendFeed() async {
     String dataString = await _loadAsset('assets/sample_data/sample_friends_feed.json');
     Map<String, dynamic> json = jsonDecode(dataString);
@@ -33,6 +67,7 @@ class MockFooderlichService {
     }
   }
 
+  // Get the sample recipe json to display in ui
   Future<List<SimpleRecipe>> getRecipes() async {
     String dataString = await _loadAsset('assets/sample_data/sample_recipes.json');
     Map<String, dynamic> json = jsonDecode(dataString);
@@ -48,6 +83,7 @@ class MockFooderlichService {
     }
   }
 
+  // Loads sample json data from file system
   Future<String> _loadAsset(String path) async {
     return await rootBundle.loadString(path);
   }
