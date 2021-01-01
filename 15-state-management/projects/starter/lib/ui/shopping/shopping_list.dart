762ddb4061e8fc1896e3e01d4f13f4f4b1f6f29a
Kevin Moore <kevindavmoore@gmail.com>
2021-01-01 11:14:04 -0800

    1-1-21 Add Todos

--- 15-state-management/projects/starter/lib/ui/shopping/shopping_list.dart
+++ 15-state-management/projects/starter/lib/ui/shopping/shopping_list.dart
@@ -30,18 +30,22 @@
 import 'package:flutter/material.dart';
 
 class ShoppingList extends StatelessWidget {
+  // TODO 1
   final List<String> ingredients = List<String>();
 
   @override
   Widget build(BuildContext context) {
+    // TODO 2
     return ListView.builder(
         itemCount: ingredients.length,
         itemBuilder: (BuildContext context, int index) {
           return CheckboxListTile(
             value: false,
+            // TODO 3
             title: Text(ingredients[index]),
             onChanged: (newValue) {},
           );
         });
+    // TODO 4
   }
 }
