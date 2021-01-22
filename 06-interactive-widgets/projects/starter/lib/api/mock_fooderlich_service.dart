1445a2c4e73b9307dafa68b0180babfa661a1583
Stef-GMS <GeekMeSpeak@gmail.com>
2021-01-21 23:37:25 -0800

    Addressed linting errors

--- 06-interactive-widgets/projects/starter/lib/api/mock_fooderlich_service.dart
+++ 06-interactive-widgets/projects/starter/lib/api/mock_fooderlich_service.dart
@@ -1,40 +1,9 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import '../models/models.dart';
 
 // Mock recipe service that grabs sample json data to mock recipe request/response
 class MockFooderlichService {
-
   // Batch request that gets both today recipes and friend's feed
   Future<ExploreData> getExploreData() async {
     var todayRecipes = await _getTodayRecipes();
@@ -48,7 +17,9 @@ class MockFooderlichService {
     // Simulate api request wait time
     await Future.delayed(Duration(milliseconds: 300));
     // Load json from file system
-    String dataString = await _loadAsset('assets/sample_data/sample_explore_recipes.json');
+    String dataString = await _loadAsset(
+      'assets/sample_data/sample_explore_recipes.json',
+    );
     // Decode to json
     Map<String, dynamic> json = jsonDecode(dataString);
 
@@ -69,7 +40,8 @@ class MockFooderlichService {
     // Simulate api request wait time
     await Future.delayed(Duration(milliseconds: 300));
     // Load json from file system
-    String dataString = await _loadAsset('assets/sample_data/sample_friends_feed.json');
+    String dataString =
+        await _loadAsset('assets/sample_data/sample_friends_feed.json');
     // Decode to json
     Map<String, dynamic> json = jsonDecode(dataString);
 
@@ -90,7 +62,8 @@ class MockFooderlichService {
     // Simulate api request wait time
     await Future.delayed(Duration(milliseconds: 300));
     // Load json from file system
-    String dataString = await _loadAsset('assets/sample_data/sample_recipes.json');
+    String dataString =
+        await _loadAsset('assets/sample_data/sample_recipes.json');
     // Decode to json
     Map<String, dynamic> json = jsonDecode(dataString);
 
--- 06-interactive-widgets/projects/starter/lib/models/explore_recipe.dart
+++ 06-interactive-widgets/projects/starter/lib/models/explore_recipe.dart
@@ -1,40 +1,10 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 part 'ingredient.dart';
 part 'instruction.dart';
 
 class RecipeCardType {
-  static final String card1 = "card1";
-  static final String card2 = "card2";
-  static final String card3 = "card3";
+  static final String card1 = 'card1';
+  static final String card2 = 'card2';
+  static final String card3 = 'card3';
 }
 
 class ExploreRecipe {
@@ -105,4 +75,3 @@ class ExploreRecipe {
     }
   }
 }
-
