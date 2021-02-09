482a5b835027a7d9b588a39c8d9363cc575d9789
Cesare Rocchi <cesarerocchi@gmail.com>
2021-02-09 23:21:09 +0100

    FPE - Fix line length and double quotes warnings

--- 04-understanding-widgets/projects/starter/lib/card2.dart
+++ 04-understanding-widgets/projects/starter/lib/card2.dart
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'package:flutter/material.dart';
 
 import 'author_card.dart';
@@ -40,14 +10,15 @@ class Card2 extends StatelessWidget {
       child: Container(
         constraints: BoxConstraints.expand(width: 350, height: 450),
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage("assets/mag5.png"), fit: BoxFit.cover),
+            image: DecorationImage(
+                image: AssetImage('assets/mag5.png'), fit: BoxFit.cover),
             borderRadius: BorderRadius.all(const Radius.circular(10.0))),
         child: Column(
           children: [
             AuthorCard(
-                authorName: "Mike Katz",
-                title: "Smoothie Connoisseur",
-                imageProvider: AssetImage("assets/author_katz.jpeg")),
+                authorName: 'Mike Katz',
+                title: 'Smoothie Connoisseur',
+                imageProvider: AssetImage('assets/author_katz.jpeg')),
             Expanded(
               child: Stack(
                 children: [
@@ -55,7 +26,7 @@ class Card2 extends StatelessWidget {
                     bottom: 16,
                     right: 16,
                     child: Text(
-                      "Recipe",
+                      'Recipe',
                       style: FooderlichTheme.lightTextTheme.headline1,
                     ),
                   ),
@@ -65,7 +36,7 @@ class Card2 extends StatelessWidget {
                     child: RotatedBox(
                       quarterTurns: 3,
                       child: Text(
-                        "Smoothies",
+                        'Smoothies',
                         style: FooderlichTheme.lightTextTheme.headline1,
                       ),
                     ),
--- 04-understanding-widgets/projects/starter/lib/explore_card.dart
+++ 04-understanding-widgets/projects/starter/lib/explore_card.dart
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'package:flutter/material.dart';
 import 'package:fooderlich/fooderlich_theme.dart';
 
@@ -38,7 +8,8 @@ class Card3 extends StatelessWidget {
       child: Container(
         constraints: BoxConstraints.expand(width: 350, height: 450),
         decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/mag2.png"), fit: BoxFit.cover),
+          image: DecorationImage(
+              image: AssetImage('assets/mag2.png'), fit: BoxFit.cover),
           borderRadius: BorderRadius.all(const Radius.circular(10.0)),
         ),
         child: Stack(
@@ -56,7 +27,8 @@ class Card3 extends StatelessWidget {
                 children: [
                   Icon(Icons.book, color: Colors.white, size: 40),
                   SizedBox(height: 8),
-                  Text("Recipe Trends", style: FooderlichTheme.darkTextTheme.headline2),
+                  Text('Recipe Trends',
+                      style: FooderlichTheme.darkTextTheme.headline2),
                   SizedBox(height: 30),
                 ],
               ),
@@ -67,21 +39,24 @@ class Card3 extends StatelessWidget {
                 spacing: 12,
                 children: [
                   Chip(
-                    label: Text("Healthy", style: FooderlichTheme.darkTextTheme.bodyText1),
+                    label: Text('Healthy',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
                     backgroundColor: Colors.black.withOpacity(0.7),
                     onDeleted: () {
-                      print("delete");
+                      print('delete');
                     },
                   ),
                   Chip(
-                    label: Text("Vegan", style: FooderlichTheme.darkTextTheme.bodyText1),
+                    label: Text('Vegan',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
                     backgroundColor: Colors.black.withOpacity(0.7),
                     onDeleted: () {
-                      print("delete");
+                      print('delete');
                     },
                   ),
                   Chip(
-                    label: Text("Carrots", style: FooderlichTheme.darkTextTheme.bodyText1),
+                    label: Text('Carrots',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
                     backgroundColor: Colors.black.withOpacity(0.7),
                   ),
                 ],
