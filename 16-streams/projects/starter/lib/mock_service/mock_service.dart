912c83f97bc88a8b1b5c271d94585196b960770e
Kevin Moore <kevindavmoore@gmail.com>
2021-02-16 13:33:55 -0800

    Add ovveride

--- 16-streams/projects/starter/lib/mock_service/mock_service.dart
+++ 16-streams/projects/starter/lib/mock_service/mock_service.dart
@@ -22,6 +22,7 @@ class MockService {
     _currentRecipes2 = APIRecipeQuery.fromJson(jsonDecode(jsonString));
   }
 
+  @override
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
       String query, int from, int to) {
     switch (nextRecipe.nextInt(2)) {
