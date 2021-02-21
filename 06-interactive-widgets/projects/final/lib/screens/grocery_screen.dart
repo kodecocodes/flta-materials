8815ceb80e8d79e8ef8ad0738be726d50eb5faff
Stef-GMS <GeekMeSpeak@gmail.com>
2021-02-21 02:54:30 -0800

    Added trailing commas

--- 06-interactive-widgets/projects/final/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/final/lib/screens/grocery_screen.dart
@@ -14,30 +14,32 @@ class GroceryScreen extends StatelessWidget {
     return Scaffold(
       // 6
       floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            // 1
-            final manager = Provider.of<GroceryManager>(
-              context, 
-              listen: false);
-            // 2
-            Navigator.push(
-              context,
-              // 3
-              MaterialPageRoute(
-                // 4
-                builder: (context) => GroceryItemScreen(
-                  // 5
-                  onCreate: (item) {
-                    // 6
-                    manager.addItem(item);
-                    // 7
-                    Navigator.pop(context);
-                  },
-                )));
-          },),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // 1
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          // 2
+          Navigator.push(
+            context,
+            // 3
+            MaterialPageRoute(
+              // 4
+              builder: (context) => GroceryItemScreen(
+                // 5
+                onCreate: (item) {
+                  // 6
+                  manager.addItem(item);
+                  // 7
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          );
+        },
+      ),
       // 7
-      body: buildGroceryScreen(),);
+      body: buildGroceryScreen(),
+    );
   }
 
   Widget buildGroceryScreen() {
@@ -45,13 +47,14 @@ class GroceryScreen extends StatelessWidget {
     return Consumer<GroceryManager>(
       // 2
       builder: (context, manager, child) {
-      // 3
-      if (manager.groceryItems.isNotEmpty) {
-        return GroceryListScreen(manager: manager);
-      } else {
-        // 4
-        return const EmptyGroceryScreen();
-      }
-    },);
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          return GroceryListScreen(manager: manager);
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
 }
