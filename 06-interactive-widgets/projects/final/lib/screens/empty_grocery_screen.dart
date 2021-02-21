b189ed78d7fc8eab4b11abaa423c1d268327c439
Stef-GMS <GeekMeSpeak@gmail.com>
2021-02-20 18:15:10 -0800

    Changed aspect ratio to use docs format of width / height

--- 06-interactive-widgets/projects/final/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/final/lib/screens/empty_grocery_screen.dart
@@ -12,29 +12,37 @@ class EmptyGroceryScreen extends StatelessWidget {
       padding: const EdgeInsets.all(30.0),
       // 2
       child: Center(
-          // 3
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AspectRatio(
-                aspectRatio: 1,
-                child: Image.asset('assets/fooderlich_assets/empty_list.png'),),
-              const SizedBox(height: 16),
-              const Text('No Groceries', style: TextStyle(fontSize: 21),),
-              const SizedBox(height: 16),
-              const Text(
-                'Shopping for ingredients? Write them down!', 
-                textAlign: TextAlign.center,),
-              MaterialButton(
-                textColor: Colors.white,
-                child: const Text('Browse Recipes'),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                color: Colors.green,
-                onPressed: () {
-                  Provider.of<TabManager>(context, listen: false).goToRecipes();
-              },),
-            ],),),
+        // 3
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AspectRatio(
+              aspectRatio: 1 / 1,
+              child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'No Groceries',
+              style: TextStyle(fontSize: 21),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Shopping for ingredients? Write them down!',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+              textColor: Colors.white,
+              child: const Text('Browse Recipes'),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              color: Colors.green,
+              onPressed: () {
+                Provider.of<TabManager>(context, listen: false).goToRecipes();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
