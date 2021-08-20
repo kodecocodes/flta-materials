1990881d6c3511d7d52740ce78b47c6ab41ee96f
Kevin Moore <kevindavmoore@gmail.com>
2021-08-19 21:32:29 -0700

    Null Safety

--- 10-serialization-with-json/projects/final/lib/ui/recipes/recipe_list.dart
+++ 10-serialization-with-json/projects/final/lib/ui/recipes/recipe_list.dart
@@ -11,7 +11,8 @@ import '../widgets/custom_dropdown.dart';
 import 'recipe_details.dart';
 
 class RecipeList extends StatefulWidget {
-  const RecipeList({Key key}) : super(key: key);
+  const RecipeList({Key? key}) : super(key: key);
+
   @override
   _RecipeListState createState() => _RecipeListState();
 }
@@ -19,8 +20,9 @@ class RecipeList extends StatefulWidget {
 class _RecipeListState extends State<RecipeList> {
   static const String prefSearchKey = 'previousSearches';
 
-  TextEditingController searchTextController;
-  final _scrollController = ScrollController();
+  late TextEditingController searchTextController;
+  final ScrollController _scrollController = ScrollController();
+  // TODO: Replace with new API class
   List currentSearchList = [];
   int currentCount = 0;
   int currentStartPosition = 0;
@@ -30,7 +32,7 @@ class _RecipeListState extends State<RecipeList> {
   bool loading = false;
   bool inErrorState = false;
   List<String> previousSearches = <String>[];
-  APIRecipeQuery _currentRecipes1;
+  APIRecipeQuery? _currentRecipes1 = null;
 
   @override
   void initState() {
@@ -80,8 +82,10 @@ class _RecipeListState extends State<RecipeList> {
   void getPreviousSearches() async {
     final prefs = await SharedPreferences.getInstance();
     if (prefs.containsKey(prefSearchKey)) {
-      previousSearches = prefs.getStringList(prefSearchKey);
-      if (previousSearches == null) {
+      final searches = prefs.getStringList(prefSearchKey);
+      if (searches != null) {
+        previousSearches = searches;
+      } else {
         previousSearches = <String>[];
       }
     }
@@ -192,12 +196,12 @@ class _RecipeListState extends State<RecipeList> {
   }
 
   Widget _buildRecipeLoader(BuildContext context) {
-    if (_currentRecipes1 == null || _currentRecipes1.hits == null) {
+    if (_currentRecipes1 == null || _currentRecipes1?.hits == null) {
       return Container();
     }
     // Show a loading indicator while waiting for the movies
     return Center(
-      child: _buildRecipeCard(context, _currentRecipes1.hits, 0),
+      child: _buildRecipeCard(context, _currentRecipes1!.hits, 0),
     );
   }
 
