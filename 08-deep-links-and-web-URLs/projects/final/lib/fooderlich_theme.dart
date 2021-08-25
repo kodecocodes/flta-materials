dac1d4f2bb6178c7ad947d07b9b7e0de10b8942d
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-25 22:41:51 +0700

    Update themes file

--- 08-deep-links-and-web-URLs/projects/final/lib/fooderlich_theme.dart
+++ 08-deep-links-and-web-URLs/projects/final/lib/fooderlich_theme.dart
@@ -4,57 +4,71 @@ import 'package:google_fonts/google_fonts.dart';
 class FooderlichTheme {
   static TextTheme lightTextTheme = TextTheme(
     bodyText1: GoogleFonts.openSans(
-      fontSize: 14.0, 
-      fontWeight: FontWeight.w700, 
-      color: Colors.black),
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
     headline1: GoogleFonts.openSans(
-      fontSize: 32.0, 
-      fontWeight: FontWeight.bold, 
-      color: Colors.black),
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
     headline2: GoogleFonts.openSans(
-      fontSize: 21.0, 
-      fontWeight: FontWeight.w700, 
-      color: Colors.black),
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
     headline3: GoogleFonts.openSans(
-      fontSize: 16.0, 
-      fontWeight: FontWeight.w600, 
-      color: Colors.black),
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
     headline6: GoogleFonts.openSans(
-      fontSize: 20.0, 
-      fontWeight: FontWeight.w600, 
-      color: Colors.black),
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
   );
 
   static TextTheme darkTextTheme = TextTheme(
     bodyText1: GoogleFonts.openSans(
-      fontSize: 14.0, 
-      fontWeight: FontWeight.w700, 
-      color: Colors.white),
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
     headline1: GoogleFonts.openSans(
-      fontSize: 32.0, 
-      fontWeight: FontWeight.bold, 
-      color: Colors.white),
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
     headline2: GoogleFonts.openSans(
-      fontSize: 21.0, 
-      fontWeight: FontWeight.w700, 
-      color: Colors.white),
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
     headline3: GoogleFonts.openSans(
-      fontSize: 16.0, 
-      fontWeight: FontWeight.w600, 
-      color: Colors.white),
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
     headline6: GoogleFonts.openSans(
-      fontSize: 20.0, 
-      fontWeight: FontWeight.w600, 
-      color: Colors.white),
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
   );
 
   static ThemeData light() {
     return ThemeData(
       brightness: Brightness.light,
-      primaryColor: Colors.white,
-      dividerColor: Colors.black,
-      textSelectionTheme:
-          const TextSelectionThemeData(selectionColor: Colors.green),
+      appBarTheme: const AppBarTheme(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.green),
       textTheme: lightTextTheme,
     );
   }
@@ -62,8 +76,14 @@ class FooderlichTheme {
   static ThemeData dark() {
     return ThemeData(
       brightness: Brightness.dark,
-      primaryColor: Colors.grey[900],
-      dividerColor: Colors.white,
+      appBarTheme: AppBarTheme(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.grey[900]),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.green),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.green),
       textTheme: darkTextTheme,
     );
   }
