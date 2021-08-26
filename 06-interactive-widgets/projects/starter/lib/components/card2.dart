b9f69e65df9948d56884cdba8bc3f4d49299196d
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-26 23:36:06 +0700

    starter project chap 6 null safety

--- 06-interactive-widgets/projects/starter/lib/components/card2.dart
+++ 06-interactive-widgets/projects/starter/lib/components/card2.dart
@@ -7,7 +7,7 @@ import 'author_card.dart';
 class Card2 extends StatelessWidget {
   final ExploreRecipe recipe;
 
-  const Card2({Key key, this.recipe}) : super(key: key);
+  const Card2({Key? key, required this.recipe}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
