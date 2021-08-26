fe76fea51f7dd60f28185ea26c73dd75d5abf893
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-27 00:29:41 +0700

    null safety starter/final

--- 03-basic-widgets/projects/final/lib/circle_image.dart
+++ 03-basic-widgets/projects/final/lib/circle_image.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class CircleImage extends StatelessWidget {
   // 1
   const CircleImage({
-    Key key,
+    Key? key,
     this.imageProvider,
     this.imageRadius = 20,
   }) : super(key: key);
 
   // 2
   final double imageRadius;
-  final ImageProvider imageProvider;
+  final ImageProvider? imageProvider;
 
   @override
   Widget build(BuildContext context) {
