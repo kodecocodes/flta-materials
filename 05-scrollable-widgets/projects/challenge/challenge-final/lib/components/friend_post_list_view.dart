47395048437bc796943403690e2f8c4353dcc7a4
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-28 11:42:54 +0700

    update challenge final / starter files to null safety, update json files to include id

--- 05-scrollable-widgets/projects/challenge/challenge-final/lib/components/friend_post_list_view.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-final/lib/components/friend_post_list_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import '../models/models.dart';
-import 'components.dart';
+import '../components/components.dart';
 
 class FriendPostListView extends StatelessWidget {
   final List<Post> friendPosts;
 
-  const FriendPostListView({Key key, this.friendPosts}) : super(key: key);
+  const FriendPostListView({
+    Key? key, 
+    required this.friendPosts}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
