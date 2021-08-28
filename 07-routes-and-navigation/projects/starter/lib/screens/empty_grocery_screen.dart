3853719e1654351238f257e7caf052f0274687ac
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-28 10:59:33 +0700

    fix alignment issues

--- 07-routes-and-navigation/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 07-routes-and-navigation/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -11,11 +11,12 @@ class EmptyGroceryScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AspectRatio(
-              aspectRatio: 1 / 1,
-              child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
             ),
-            const SizedBox(height: 8.0),
             const Text(
               'No Groceries',
               style: TextStyle(fontSize: 21.0),
