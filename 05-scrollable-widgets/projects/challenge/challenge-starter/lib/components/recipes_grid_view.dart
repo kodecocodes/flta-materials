3ccdf66a90f81c98ab0ee937ad01fb231210285a
Vincent Ngo <vincentvyngo@yahoo.com>
2021-08-28 11:27:48 +0700

    update challenge starter

--- 05-scrollable-widgets/projects/challenge/challenge-starter/lib/components/recipes_grid_view.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-starter/lib/components/recipes_grid_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'components.dart';
+import '../components/components.dart';
 import '../models/models.dart';
 
 class RecipesGridView extends StatelessWidget {
   final List<SimpleRecipe> recipes;
 
-  const RecipesGridView({Key key, this.recipes}) : super(key: key);
+  const RecipesGridView({Key? key, required this.recipes}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
