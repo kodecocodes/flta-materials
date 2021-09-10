28bb4c991bd5e3aba42f04c83f484690166026a2
Cesare <cesarerocchi@gmail.com>
2021-09-10 23:03:01 +0200

    FPE - Update starter project

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  // 1
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
+  // 2
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO: Create theme
     // TODO: Apply Home widget
+    // 3
     return MaterialApp(
       // TODO: Add theme
       title: 'Fooderlich',
+      // 4
       home: Scaffold(
         // TODO: Style the title
         appBar: AppBar(title: const Text('Fooderlich')),
