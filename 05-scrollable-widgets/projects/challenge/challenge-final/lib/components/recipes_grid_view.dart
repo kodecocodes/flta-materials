40230e0aac5274d3862829d123f36169e3a2bdc5
Stef-GMS <stephanie@GeekMeSpeak.com>
2021-09-11 21:39:58 -0700

    TE Edits: Added trailing commas

--- 05-scrollable-widgets/projects/challenge/challenge-final/lib/components/recipes_grid_view.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-final/lib/components/recipes_grid_view.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
-import 'components.dart';
+
 import '../models/models.dart';
+import 'components.dart';
 
 class RecipesGridView extends StatelessWidget {
   final List<SimpleRecipe> recipes;
 
-  const RecipesGridView({Key? key, required this.recipes}) : super(key: key);
+  const RecipesGridView({
+    Key? key,
+    required this.recipes,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
-        child: GridView.builder(
-            itemCount: recipes.length,
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 500.0),
-            itemBuilder: (context, index) {
-              final simpleRecipe = recipes[index];
-              return RecipeThumbnail(recipe: simpleRecipe);
-            }));
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      child: GridView.builder(
+        itemCount: recipes.length,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 500.0),
+        itemBuilder: (context, index) {
+          final simpleRecipe = recipes[index];
+          return RecipeThumbnail(recipe: simpleRecipe);
+        },
+      ),
+    );
   }
 }
