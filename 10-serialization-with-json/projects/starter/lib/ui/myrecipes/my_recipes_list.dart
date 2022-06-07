afbac9075e761a8a038f4234ab385b2b40379fb0
Kevin Moore <kevindavmoore@gmail.com>
2022-06-06 20:17:16 -0700

    Chapter 10 Updates

--- 10-serialization-with-json/projects/starter/lib/ui/myrecipes/my_recipes_list.dart
+++ 10-serialization-with-json/projects/starter/lib/ui/myrecipes/my_recipes_list.dart
@@ -37,8 +37,32 @@ class _MyRecipesListState extends State<MyRecipesList> {
           return SizedBox(
             height: 100,
             child: Slidable(
-              actionPane: const SlidableDrawerActionPane(),
-              actionExtentRatio: 0.25,
+                      startActionPane: ActionPane(
+                        motion: const DrawerMotion(),
+                        extentRatio: 0.25,
+                        children: [
+                          SlidableAction(
+                              label: 'Delete',
+                              backgroundColor: Colors.transparent,
+                              foregroundColor: Colors.black,
+                              icon: Icons.delete,
+                              onPressed: (_) {}
+                          )
+                        ],
+                      ),
+                      endActionPane: ActionPane(
+                        motion: const DrawerMotion(),
+                        extentRatio: 0.25,
+                        children: [
+                          SlidableAction(
+                              label: 'Delete',
+                              backgroundColor: Colors.transparent,
+                              foregroundColor: Colors.black,
+                              icon: Icons.delete,
+                              onPressed: (_) {}
+                          )
+                        ],
+                      ),
               child: Card(
                 elevation: 1.0,
                 shape: RoundedRectangleBorder(
@@ -63,24 +87,6 @@ class _MyRecipesListState extends State<MyRecipesList> {
                   ),
                 ),
               ),
-              actions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO 7
-                    onTap: () {})
-              ],
-              secondaryActions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO 8
-                    onTap: () {})
-              ],
             ),
           );
         });
