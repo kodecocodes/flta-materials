b4a177707cc336337ede21bb466d0c55a833438c
Cesare Rocchi <cesarerocchi@gmail.com>
2022-08-02 22:43:03 +0200

    FPE - Reinstate Flutter 3.0.5 compatibility

--- 11-networking-in-flutter/projects/final/lib/ui/recipes/recipe_details.dart
+++ 11-networking-in-flutter/projects/final/lib/ui/recipes/recipe_details.dart
@@ -65,7 +65,7 @@ class RecipeDetails extends StatelessWidget {
                 Center(
                   child: ElevatedButton.icon(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: green,
+                      primary: green,
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16.0)),
                     ),
