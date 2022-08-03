bce6a2cbdb17bccae2fe73ade4015fb767287c2c
Stef-GMS <stephanie@GeekMeSpeak.com>
2022-08-02 21:44:28 -0700

    Trailing comma

--- 13-managing-state/projects/starter/lib/ui/myrecipes/my_recipes_list.dart
+++ 13-managing-state/projects/starter/lib/ui/myrecipes/my_recipes_list.dart
@@ -30,66 +30,67 @@ class _MyRecipesListState extends State<MyRecipesList> {
   Widget _buildRecipeList(BuildContext context) {
     // TODO: Add Consumer
     return ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (BuildContext context, int index) {
-          // TODO: Add recipe definition
-          return SizedBox(
-            height: 100,
-            child: Slidable(
-              startActionPane: ActionPane(
-                motion: const DrawerMotion(),
-                extentRatio: 0.25,
-                children: [
-                  SlidableAction(
-                    label: 'Delete',
-                    backgroundColor: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    icon: Icons.delete,
-                    // TODO: Update first onPressed
-                    onPressed: (context) {},
-                  ),
-                ],
-              ),
-              endActionPane: ActionPane(
-                motion: const DrawerMotion(),
-                extentRatio: 0.25,
-                children: [
-                  SlidableAction(
-                    label: 'Delete',
-                    backgroundColor: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    icon: Icons.delete,
-                    // TODO: Update second onPressed
-                    onPressed: (context) {},
-                  ),
-                ],
-              ),
-              child: Card(
-                elevation: 1.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+      itemCount: recipes.length,
+      itemBuilder: (BuildContext context, int index) {
+        // TODO: Add recipe definition
+        return SizedBox(
+          height: 100,
+          child: Slidable(
+            startActionPane: ActionPane(
+              motion: const DrawerMotion(),
+              extentRatio: 0.25,
+              children: [
+                SlidableAction(
+                  label: 'Delete',
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.black,
+                  icon: Icons.delete,
+                  // TODO: Update first onPressed
+                  onPressed: (context) {},
                 ),
-                color: Colors.white,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      // TODO: Replace with image from recipe
-                      leading: Image.asset(
-                        'assets/images/pizza_w700.png',
-                        height: 200,
-                        width: 200,
-                      ),
-                      // TODO: Replace title hardcoding
-                      title: const Text('Chicken Vesuvio'),
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const DrawerMotion(),
+              extentRatio: 0.25,
+              children: [
+                SlidableAction(
+                  label: 'Delete',
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.black,
+                  icon: Icons.delete,
+                  // TODO: Update second onPressed
+                  onPressed: (context) {},
+                ),
+              ],
+            ),
+            child: Card(
+              elevation: 1.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              color: Colors.white,
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    // TODO: Replace with image from recipe
+                    leading: Image.asset(
+                      'assets/images/pizza_w700.png',
+                      height: 200,
+                      width: 200,
                     ),
+                    // TODO: Replace title hardcoding
+                    title: const Text('Chicken Vesuvio'),
                   ),
                 ),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
     // TODO: Add final brace and parenthesis
   }
 
