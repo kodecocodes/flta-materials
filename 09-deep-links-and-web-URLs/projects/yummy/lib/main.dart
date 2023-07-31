9e633c39e6803662642be03b9213bcae86da894b
Vincent Ngo <vincentvyngo@yahoo.com>
2023-08-01 00:50:32 +0700

    remove all OS projects, and just add flutter project code

--- 09-deep-links-and-web-URLs/projects/yummy/lib/main.dart
+++ 09-deep-links-and-web-URLs/projects/yummy/lib/main.dart
@@ -1,125 +1,129 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
+import 'models/auth.dart';
+import 'models/orders.dart';
+import 'models/restaurant.dart';
+import 'models/shopping_cart.dart';
+import 'restaurant_page.dart';
+
+import 'constants.dart';
+import 'home.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.dark;
+  ColorSeed colorSelected = ColorSeed.blue;
+  ColorScheme? imageColorScheme = const ColorScheme.light();
+
+  /// Authentication to manage user login session
+  final YummyAuth _auth = YummyAuth();
+
+  /// Manage user's shopping cart for the items they order.
+  final ShoppingCart _shoppingCart = ShoppingCart();
+
+  /// Manage user's orders submitted
+  final OrdersManager _orders = OrdersManager();
+
+  late final _router = GoRouter(
+    debugLogDiagnostics: true,
+    initialLocation: '/login',
+    redirect: _appRedirect,
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) =>
+            Login(onLogIn: (Credentials credentials) async {
+          _auth
+              .signIn(credentials.username, credentials.password)
+              .then((_) => context.go('/'));
+        }),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return Home(
+                auth: _auth,
+                shoppingCart: _shoppingCart,
+                ordersManager: _orders,
+                handleBrightnessChange: handleBrightnessChange,
+                handleColorSelect: handleColorSelect,
+                colorSelected: colorSelected,
+                tab: int.tryParse(state.queryParameters['tab'] ?? '') ?? 0);
+          },
+          routes: [
+            GoRoute(
+                path: 'restaurant/:id',
+                builder: (context, state) {
+                  final id = 
+                    int.tryParse(state.pathParameters['id'] ?? '') ?? 0;
+                  final restaurant = restaurants[id];
+                  return RestaurantPage(
+                    restaurant: restaurant,
+                    shoppingCart: _shoppingCart,
+                    ordersManager: _orders,
+                  );
+                }),
+          ]),
+    ],
+  );
+
+  String? _appRedirect(BuildContext context, GoRouterState state) {
+    final loggedIn = _auth.loggedIn;
+    final isOnLoginPage = state.matchedLocation == '/login';
+
+    // Go to /login if the user is not signed in
+    if (!loggedIn) {
+      return '/login';
+    }
+    // Go to root of app / if the user is already signed in
+    else if (loggedIn && isOnLoginPage) {
+      return '/';
+    }
+
+    // no redirect
+    return null;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void handleBrightnessChange(bool useLightMode) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  void handleColorSelect(int value) {
+    setState(() {
+      colorSelected = ColorSeed.values[value];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+      title: 'Yummy',
+      themeMode: themeMode,
+      theme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ 09-deep-links-and-web-URLs/projects/yummy/lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  final String firstName;
+  final String lastName;
+  final String role;
+  final String profileImageUrl;
+  final int points;
+  final bool darkMode;
+
+  User({
+    required this.firstName,
+    required this.lastName,
+    required this.role,
+    required this.profileImageUrl,
+    required this.points,
+    required this.darkMode,
+  });
+}
