9da716825b7f9c61eb031ed1339b6d8de24ed6fb
Vincent Ngo <vincentvyngo@yahoo.com>
2023-08-15 23:38:40 +0700

    final project for chap 3

--- 03-basic-widgets/projects/final/lib/main.dart
+++ 03-basic-widgets/projects/final/lib/main.dart
@@ -1,21 +1,57 @@
-import 'package:flutter/material.dart';
 
-import 'fooderlich_theme.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
 import 'home.dart';
 
 void main() {
-  runApp(const Fooderlich());
+  runApp(const MyApp());
 }
 
-class Fooderlich extends StatelessWidget {
-  const Fooderlich({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.dark;
+  ColorSeed colorSelected = ColorSeed.blue;
+  ColorScheme? imageColorScheme = const ColorScheme.light();
+
+
+  void handleBrightnessChange(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  void handleColorSelect(int value) {
+    setState(() {
+      colorSelected = ColorSeed.values[value];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
     return MaterialApp(
-      theme: theme,
-      title: 'Fooderlich',
-      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      title: 'Yummy',
+      themeMode: themeMode,
+      theme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: Home(
+          handleBrightnessChange: handleBrightnessChange,
+          handleColorSelect: handleColorSelect,
+          colorSelected: colorSelected),
     );
   }
 }
