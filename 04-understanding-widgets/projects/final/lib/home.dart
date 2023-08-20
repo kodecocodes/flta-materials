5f665d1cc541631d91bc931c4028e093e9aec64f
Vincent Ngo <vincentvyngo@yahoo.com>
2023-08-20 23:22:15 +0700

    update chapter 4

--- 04-understanding-widgets/projects/final/lib/home.dart
+++ 04-understanding-widgets/projects/final/lib/home.dart
@@ -1,59 +1,184 @@
 import 'package:flutter/material.dart';
+import 'components/category_card.dart';
+import 'components/post_card.dart';
+import 'components/restaurant_landscape_card.dart';
+import 'constants.dart';
 
-import 'card1.dart';
-import 'card2.dart';
-import 'card3.dart';
+import 'models/food_category.dart';
+import 'models/post.dart';
+import 'models/restaurant.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class _BrightnessButton extends StatelessWidget {
+  const _BrightnessButton({
+    required this.handleBrightnessChange,
+  });
 
-  @override
-  HomeState createState() => HomeState();
-}
-
-class HomeState extends State<Home> {
-  int _selectedIndex = 0;
-
-  static List<Widget> pages = <Widget>[
-    const Card1(),
-    const Card2(),
-    const Card3(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  final Function handleBrightnessChange;
+  final bool showTooltipBelow = true;
 
   @override
   Widget build(BuildContext context) {
+    final isBright = Theme.of(context).brightness == Brightness.light;
+    return Tooltip(
+      preferBelow: showTooltipBelow,
+      message: 'Toggle brightness',
+      child: IconButton(
+        icon: isBright
+            ? const Icon(Icons.dark_mode_outlined)
+            : const Icon(Icons.light_mode_outlined),
+        onPressed: () => handleBrightnessChange(!isBright),
+      ),
+    );
+  }
+}
+
+class _ColorSeedButton extends StatelessWidget {
+  const _ColorSeedButton({
+    required this.handleColorSelect,
+    required this.colorSelected,
+  });
+
+  final void Function(int) handleColorSelect;
+  final ColorSeed colorSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      icon: Icon(
+        Icons.opacity_outlined,
+        color: Theme.of(context).colorScheme.onSurfaceVariant,
+      ),
+      tooltip: 'Select a Seed Color',
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      itemBuilder: (context) {
+        return List.generate(ColorSeed.values.length, (index) {
+          final currentColor = ColorSeed.values[index];
+
+          return PopupMenuItem(
+            value: index,
+            enabled: currentColor != colorSelected,
+            child: Wrap(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Icon(
+                    currentColor == colorSelected
+                        ? Icons.opacity_rounded
+                        : Icons.opacity_outlined,
+                    color: currentColor.color,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(currentColor.label),
+                ),
+              ],
+            ),
+          );
+        });
+      },
+      onSelected: handleColorSelect,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+    required this.handleBrightnessChange,
+    required this.handleColorSelect,
+    required this.colorSelected,
+  });
+
+  final ColorSeed colorSelected;
+  final void Function(bool useLightMode) handleBrightnessChange;
+  final void Function(int value) handleColorSelect;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  ThemeMode themeMode = ThemeMode.system;
+  ColorSeed colorSelected = ColorSeed.orange;
+  ColorScheme? imageColorScheme = const ColorScheme.light();
+  int tab = 0;
+
+  void handleBrightnessChange(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  void handleColorSelect(int value) {
+    setState(() {
+      colorSelected = ColorSeed.values[value];
+    });
+  }
+
+  List<NavigationDestination> appBarDestinations = const [
+    NavigationDestination(
+      tooltip: '',
+      icon: Icon(Icons.credit_card),
+      label: 'Card',
+      selectedIcon: Icon(Icons.credit_card),
+    ),
+    NavigationDestination(
+      tooltip: '',
+      icon: Icon(Icons.credit_card),
+      label: 'Card2',
+      selectedIcon: Icon(Icons.credit_card),
+    ),
+    NavigationDestination(
+      tooltip: '',
+      icon: Icon(Icons.credit_card),
+      label: 'Card3',
+      selectedIcon: Icon(Icons.credit_card),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = <Widget>[
+      Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 300),  // Adjust as needed
+          child: CategoryCard(category: categories[0]))),
+      Center(
+        child: PostCard(post: posts[0])),
+      Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 400),  // Adjust as needed
+          child: RestaurantLandscapeCard(
+            restaurant: restaurants[0])))
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Fooderlich',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-      ),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card',
+        backgroundColor: Theme.of(context).colorScheme.background,
+        actions: [
+          _BrightnessButton(
+            handleBrightnessChange: widget.handleBrightnessChange,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card2',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card3',
+          _ColorSeedButton(
+            handleColorSelect: widget.handleColorSelect,
+            colorSelected: widget.colorSelected,
           ),
         ],
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IndexedStack(index: tab, children: pages)),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: tab,
+        onDestinationSelected: (index) {
+          setState(() {
+            tab = index;
+          });
+        },
+        destinations: appBarDestinations,
       ),
     );
   }
---
+++ 04-understanding-widgets/projects/starter/lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+enum ColorSeed {
+  baseColor('M3 Baseline', Color(0xff6750a4)),
+  indigo('Indigo', Colors.indigo),
+  blue('Blue', Colors.blue),
+  teal('Teal', Colors.teal),
+  green('Green', Colors.green),
+  yellow('Yellow', Colors.yellow),
+  orange('Orange', Colors.orange),
+  deepOrange('Deep Orange', Colors.deepOrange),
+  pink('Pink', Colors.pink);
+
+  const ColorSeed(this.label, this.color);
+  final String label;
+  final Color color;
+}
