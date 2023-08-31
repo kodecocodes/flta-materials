8695a10a36103b3be601153a67971cb0fccd9c5b
Shree Bhagwat <shree@codeaamy.com>
2023-08-31 11:57:09 +0530

    added replace code to starter project

--- 12-networking-in-flutter/projects/starter/lib/main.dart
+++ 12-networking-in-flutter/projects/starter/lib/main.dart
@@ -6,11 +6,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lumberdash/lumberdash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'mock_service/mock_service.dart';
 import 'providers.dart';
 import 'ui/main_screen.dart';
 import 'ui/theme/theme.dart';
 import 'utils.dart';
 import 'package:logging/logging.dart' as system_log;
+
 
 Future<void> main() async {
   _setupLogging();
@@ -21,6 +23,7 @@ Future<void> main() async {
     await DesktopWindow.setMinWindowSize(const Size(260, 600));
   }
   final sharedPrefs = await SharedPreferences.getInstance();
+  final service = await MockService.create();
   runApp(ProviderScope(overrides: [
     sharedPrefProvider.overrideWithValue(sharedPrefs),
   ], child: const MyApp()));
