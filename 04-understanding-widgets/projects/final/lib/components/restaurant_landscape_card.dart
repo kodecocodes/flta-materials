0119f9a32d841202bdbf21449c822f402ef4533e
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-10-21 18:22:44 -0700

    Added trailing commas

--- 04-understanding-widgets/projects/final/lib/components/restaurant_landscape_card.dart
+++ 04-understanding-widgets/projects/final/lib/components/restaurant_landscape_card.dart
@@ -12,12 +12,12 @@ class RestaurantLandscapeCard extends StatefulWidget {
 
   @override
   State<RestaurantLandscapeCard> createState() =>
-        _RestaurantLandscapeCardState();
+      _RestaurantLandscapeCardState();
 }
 
 class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
   bool _isFavorited = false;
-  
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context)
@@ -28,21 +28,27 @@ class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
         mainAxisSize: MainAxisSize.min,
         children: [
           ClipRRect(
-            borderRadius:
-                const BorderRadius.vertical(top: Radius.circular(8.0)),
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(8.0),
+            ),
             child: AspectRatio(
                 aspectRatio: 2,
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
-                    Image.asset(widget.restaurant.imageUrl, fit: BoxFit.cover),
+                    Image.asset(
+                      widget.restaurant.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                     Positioned(
                       top: 4.0,
                       right: 4.0,
                       child: IconButton(
-                        icon: Icon(_isFavorited
-                            ? Icons.favorite
-                            : Icons.favorite_border),
+                        icon: Icon(
+                          _isFavorited
+                              ? Icons.favorite //
+                              : Icons.favorite_border,
+                        ),
                         iconSize: 30.0,
                         color: Colors.red[400],
                         onPressed: () {
@@ -53,13 +59,15 @@ class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
                       ),
                     ),
                   ],
-                )
-              ),
+                )),
           ),
           ListTile(
             title: Text(widget.restaurant.name, style: textTheme.titleSmall),
-            subtitle: Text(widget.restaurant.attributes,
-                maxLines: 1, style: textTheme.bodySmall),
+            subtitle: Text(
+              widget.restaurant.attributes,
+              maxLines: 1,
+              style: textTheme.bodySmall,
+            ),
             onTap: () {
               // ignore: avoid_print
               print('Tap on ${widget.restaurant.name}');
