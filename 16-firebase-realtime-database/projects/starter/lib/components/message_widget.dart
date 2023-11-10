fe073eb82203a0061d3c26ea7c8859bb89df04b9
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-11-10 08:59:42 -0800

    Removed intl.dart pkg from starter

--- 16-firebase-realtime-database/projects/starter/lib/components/message_widget.dart
+++ 16-firebase-realtime-database/projects/starter/lib/components/message_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 
 class MessageWidget extends ConsumerWidget {
   // TODO: Replace MessageWidget
