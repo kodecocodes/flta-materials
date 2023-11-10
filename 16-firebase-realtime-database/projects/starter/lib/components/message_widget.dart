bbde13e10c8d1b452d2aac07bc08fdfa36d772c5
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-11-09 21:43:29 -0800

    Removed code

--- 16-firebase-realtime-database/projects/starter/lib/components/message_widget.dart
+++ 16-firebase-realtime-database/projects/starter/lib/components/message_widget.dart
@@ -2,53 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
-import '../providers.dart';
-import 'message.dart';
-
 class MessageWidget extends ConsumerWidget {
+  // TODO: Replace MessageWidget
   const MessageWidget(
-      this.message, {
-        super.key,
-      });
-
-  final Message message;
+  {
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    final userDao = ref.watch(userDaoProvider);
-    final myMessage = message.email == userDao.email();
+
+    // TODO: Add userDao and myMessage
 
     return FractionallySizedBox(
       widthFactor: 0.7,
-      alignment: myMessage ? Alignment.topRight : Alignment.topLeft,
+      // TODO: Add alignment
       child: Column(
-        crossAxisAlignment:
-        myMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        // TODO: Add crossAxisAlignment
         children: [
-          Row(
-            mainAxisAlignment:
-            myMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
-            children: [
-              // Display email of others not ones sent from device
-              !myMessage
-                  ? Text(
-                message.email,
-                style: TextStyle(
-                  color: theme.colorScheme.secondary,
-                ),
-              )
-                  : const Text(''),
-              // Display date and time message was sent
-              Text(
-                '  ${DateFormat.yMd().format(message.date)} '
-                    '${DateFormat.Hm().format(message.date)}',
-                style: TextStyle(
-                  color: theme.colorScheme.secondary,
-                ),
-              ),
-            ],
-          ),
+          // TODO: Add Row for message
           // Speech bubble
           Material(
             color: theme.colorScheme.surface,
@@ -57,16 +30,15 @@ class MessageWidget extends ConsumerWidget {
             // shadowColor: theme.colorScheme.surfaceTint,
             elevation: 1.0,
             borderRadius: BorderRadius.circular(10.0),
-            child: Padding(
+            // TODO: Remove const
+            child: const Padding(
               padding:
-              const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                  EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text(
-                    message.text,
-                    style: theme.textTheme.bodyLarge!,
-                  ),
+                  // TODO: Replace Text
+                  Text('message will go here'),
                 ],
               ),
             ),
