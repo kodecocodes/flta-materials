9ed94ddd6e3aaef67a1d06812b36b2dbf9f96e12
Alejandro Ulate <codingalecr@gmail.com>
2023-12-28 11:35:43 -0600

    Chapter 18: widget testing (#226)
    * Adding 18-widget-testing
    
    * feat: add starter project
    
    * fix: update projects
    
    * fix: gitignore issues
    
    * fix: analysis files
    
    * feat: adds final result for widget and golden tests
    
    * feat: adds challenges
    
    * fix: isolates mock ingredient name
    
    * fix: identation on golden test scenarios
    
    * fix: remove key
    
    * fix: starter project update
    
    * fix: update final project
    
    * FPE - Update starter lock files
    
    * FPE - Update final lock files
    
    * deprecated text scaler fix
    
    * fix: add challenge projects
    
    * fix: remove projects in favor of single file challenge solutions
    
    * FPE - Update starter lock files
    
    * FPE - Update final lock files
    
    ---------
    
    Co-authored-by: edith <edith@kodeco.com>
    Co-authored-by: Cesare Rocchi <cesarerocchi@gmail.com>
    Co-authored-by: Shree Bhagwat <shree@codeaamy.com>

---
+++ 18-widget-testing/projects/final/lib/ui/widgets/custom_dropdown.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomDropdownMenuItem<T> extends PopupMenuEntry<T> {
+  const CustomDropdownMenuItem(
+      {Key? key, required this.value, required this.text, this.callback})
+      : super(key: key);
+
+  final T value;
+  final String text;
+  final Function? callback;
+
+  @override
+  State<CustomDropdownMenuItem<T>> createState() =>
+      _CustomDropdownMenuItemState<T>();
+
+  @override
+  double get height => 32.0;
+
+  @override
+  bool represents(T? value) => this.value == value;
+}
+
+class _CustomDropdownMenuItemState<T> extends State<CustomDropdownMenuItem<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minWidth: 120),
+      child: InkWell(
+        onTap: () => Navigator.of(context).pop<T>(widget.value),
+        child: Container(
+          constraints: const BoxConstraints(minWidth: 30.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(
+                widget.text,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey.shade600,
+                ),
+              ),
+              trailing: GestureDetector(
+                onTap: () {
+                  if (widget.callback != null) {
+                    widget.callback!();
+                  }
+                },
+                child: SvgPicture.asset(
+                  'assets/images/dismiss.svg',
+                  colorFilter:
+                  const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+                  semanticsLabel: 'Back',
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ 18-widget-testing/projects/final/test/ui/widgets/ingredient_card_test.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:recipes/ui/widgets/ingredient_card.dart';
+
+Widget _buildWrappedWidget(Widget child) {
+  return MaterialApp(
+    home: Scaffold(
+      body: ListView(
+        children: [
+          child,
+        ],
+      ),
+    ),
+  );
+}
+
+void main() {
+  const mockIngredientName = 'colby jack cheese';
+  group('IngredientCard', () {
+    testWidgets('can build', (tester) async {
+      await tester.pumpWidget(
+        _buildWrappedWidget(IngredientCard(
+          name: mockIngredientName,
+          initiallyChecked: false,
+          evenRow: true,
+          onChecked: (isChecked) {},
+        )),
+      );
+
+      final cardFinder = find.byType(IngredientCard);
+      final titleFinder = find.text(mockIngredientName);
+
+      expect(cardFinder, findsOneWidget);
+      expect(titleFinder, findsOneWidget);
+    });
+
+    testWidgets('can be checked when tapped', (tester) async {
+      var isChecked = false;
+      await tester.pumpWidget(
+        _buildWrappedWidget(IngredientCard(
+          name: mockIngredientName,
+          initiallyChecked: isChecked,
+          evenRow: true,
+          onChecked: (newValue) {
+            isChecked = newValue;
+          },
+        )),
+      );
+
+      final cardFinder = find.byType(IngredientCard);
+
+      await tester.tap(cardFinder);
+      await tester.pumpAndSettle();
+
+      final checkboxFinder = find.byType(Checkbox);
+
+      expect(checkboxFinder, findsOneWidget);
+      expect(isChecked, isTrue);
+    });
+  });
+
+  group('Golden Tests - IngredientCard', () {
+    testGoldens('can support light theme', (tester) async {
+      final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 1)
+        ..addScenario(
+          'Light - Unchecked',
+          IngredientCard(
+            name: mockIngredientName,
+            initiallyChecked: false,
+            evenRow: true,
+            onChecked: (newValue) {},
+          ),
+        )
+        ..addScenario(
+          'Light - Checked',
+          IngredientCard(
+            name: mockIngredientName,
+            initiallyChecked: true,
+            evenRow: true,
+            onChecked: (newValue) {},
+          ),
+        )
+        ..addScenario(
+          'Light - Odd - Unchecked',
+          IngredientCard(
+            name: mockIngredientName,
+            initiallyChecked: false,
+            evenRow: false,
+            onChecked: (newValue) {},
+          ),
+        )
+        ..addScenario(
+          'Light - Odd - Checked',
+          IngredientCard(
+            name: mockIngredientName,
+            initiallyChecked: true,
+            evenRow: false,
+            onChecked: (newValue) {},
+          ),
+        );
+      await tester.pumpWidgetBuilder(
+        builder.build(),
+        wrapper: materialAppWrapper(
+          theme: ThemeData.light(),
+        ),
+      );
+      await screenMatchesGolden(tester, 'light_ingredient_card');
+    });
+  });
+}
+
+  
