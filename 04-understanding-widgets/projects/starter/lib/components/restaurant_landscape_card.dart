ad3377f86a888e39de1399723242770f27f5133d
vincentngo <vincentvyngo@yahoo.com>
2024-01-04 16:08:12 +0700

    04 understanding widgets (#222)
    * update chapter 4
    
    * add missing android files
    
    * update chap 4
    
    * update chapter 4 final and starter projects
    
    * Add TODO
    
    * Added trailing commas; chg comment
    
    * Added trailing commas
    
    * Forgot to remove note to myself
    
    * Fixed spelling
    
    * FPE - Update starter lock files
    
    * FPE - Update final lock files
    
    ---------
    
    Co-authored-by: Stef-GMS <stephanie@GeekMeSpeak.com>
    Co-authored-by: Cesare Rocchi <cesarerocchi@gmail.com>

---
+++ 04-understanding-widgets/projects/starter/lib/components/restaurant_landscape_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../models/restaurant.dart';
+
+class RestaurantLandscapeCard extends StatelessWidget {
+  final Restaurant restaurant;
+
+  const RestaurantLandscapeCard({
+    super.key,
+    required this.restaurant,
+  });
+
+  // TODO: Add _isFavorited property
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context)
+        .textTheme
+        .apply(displayColor: Theme.of(context).colorScheme.onSurface);
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(top: Radius.circular(8.0)),
+            child: AspectRatio(
+              aspectRatio: 2,
+              // TODO: Convert to a stack
+              child: Image.asset(
+                restaurant.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              restaurant.name,
+              style: textTheme.titleSmall,
+            ),
+            subtitle: Text(
+              restaurant.attributes,
+              maxLines: 1,
+              style: textTheme.bodySmall,
+            ),
+            onTap: () {
+              // ignore: avoid_print
+              print('Tap on ${restaurant.name}');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
