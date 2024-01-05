c2e78bb9fb2a9ee488856faf1b5e17a7e85be36b
Stef-GMS <stephanie@GeekMeSpeak.com>
2024-01-05 08:30:45 -0800

    23 build and release an ios app (#233)
    * Chapter updates
    
    * Remove my app dev details
    
    * Updates
    
    * Attempt to remove error
    
    * Putting changes back
    
    * Copied from chpt 22 final
    
    * Add missing files
    
    * FPE - Revert to flutter.minSdkVersion
    
    * FPE - Update lock files
    
    ---------
    
    Co-authored-by: Kevin Moore <kevindavmoore@gmail.com>
    Co-authored-by: Cesare Rocchi <cesarerocchi@gmail.com>

--- 23-build-and-release-an-ios-app/projects/starter/lib/data/models/recipe.dart
+++ 23-build-and-release-an-ios-app/projects/starter/lib/data/models/recipe.dart
@@ -1,52 +1,22 @@
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'ingredient.dart';
 
-// ignore: must_be_immutable
-class Recipe extends Equatable {
-  int? id;
-  final String? label;
-  final String? image;
-  final String? url;
-  List<Ingredient>? ingredients;
-  final double? calories;
-  final double? totalWeight;
-  final double? totalTime;
+part 'recipe.freezed.dart';
+part 'recipe.g.dart';
 
-  Recipe({
-    this.id,
-    this.label,
-    this.image,
-    this.url,
-    this.calories,
-    this.totalWeight,
-    this.totalTime,
-  });
-
-  @override
-  List<Object?> get props =>
-      [label, image, url, calories, totalWeight, totalTime];
+@freezed
+class Recipe with _$Recipe {
+  const factory Recipe({
+  int? id,
+  String? label,
+  String? image,
+  String? description,
+  @Default(false) bool bookmarked,
+  @Default(<Ingredient>[]) List<Ingredient> ingredients
+  }) = _Recipe;
 
   // Create a Recipe from JSON data
-  factory Recipe.fromJson(Map<String, dynamic> json) => Recipe(
-        id: json['recipeId'],
-        label: json['label'],
-        image: json['image'],
-        url: json['url'],
-        calories: json['calories'],
-        totalWeight: json['totalWeight'],
-        totalTime: json['totalTime'],
-      );
+  factory Recipe.fromJson(Map<String, dynamic> json) => _$RecipeFromJson(json);
 
-  // Convert our Recipe to JSON to make it easier
-  // when we store it in the database
-  Map<String, dynamic> toJson() => {
-        'recipeId': id,
-        'label': label,
-        'image': image,
-        'url': url,
-        'calories': calories,
-        'totalWeight': totalWeight,
-        'totalTime': totalTime,
-      };
 }
