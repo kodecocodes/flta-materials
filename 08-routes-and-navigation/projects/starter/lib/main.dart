e2aa576e06999b1d775f5f301b71cc71c482c51a
Cesare Rocchi <cesarerocchi@gmail.com>
2024-01-27 21:47:41 +0100

    FPE - Fix TODO in starter project

--- 08-routes-and-navigation/projects/starter/lib/main.dart
+++ 08-routes-and-navigation/projects/starter/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'constants.dart';
-// TODO: Replace home with screens.dart
 import 'home.dart';
 import '../models/models.dart';
 import 'screens/screens.dart';
